e46a6e8620fcbde18127f237010fb2a45747c71a
josefineanerland <54805600+josefineanerland@users.noreply.github.com>
2020-03-04 12:30:54 +0100

    Add files via upload

---
+++ SnakeAdventure/Food.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAdventure
+{
+    public class Food
+    {
+        public int scoreModifier = 0;
+        public string singleName = "";
+        public string multipleName = "";
+        public int Eat(int score)
+        {
+            return score + scoreModifier;
+        }
+    }
+
+    public class Mouse : Food
+    {
+        public Mouse()
+        {
+            scoreModifier = 3;
+            singleName = "Mus";
+            multipleName = "Möss";
+        }
+    }
+
+    public class Rat : Food
+    {
+        public Rat()
+        {
+            scoreModifier = 5;
+            singleName = "Råtta";
+            multipleName = "Råttor";
+        }
+    }
+
+    public class Bird : Food
+    {
+        public Bird()
+        {
+            scoreModifier = 4;
+            singleName = "Fågel";
+            multipleName = "Fåglar";
+        }
+    }
+
+    public class Berry : Food
+    {
+        public Berry()
+        {
+            scoreModifier = -3;
+            singleName = "Bär";
+            multipleName = "Bär";
+        }
+    }
+
+    public class Apple : Food
+    {
+        public Apple()
+        {
+            scoreModifier = -5;
+            singleName = "Äpple";
+            multipleName = "Äpplen";
+        }
+    }
+}
